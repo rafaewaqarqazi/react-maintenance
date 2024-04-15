18021cd6eeafb4a0ab29ea7566978e55f3c986b8
rafaewaqarqazi <rafaewaqar@gmail.com>
2024-04-15 19:12:34 +0500

    Add: maintenance page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const appLogo = process.env.REACT_APP_LOGO;
+  return (
+    <div className="App">
+      <img src={appLogo || logo} className="App-logo" alt="logo" />
+        <div className='maintenance-container'>
+            <img src={'/maintenance.png'} alt='maintenance' />
+            <h1>Under Maintenance</h1>
+        </div>
+        <p>Sorry for the inconvenience. We'll be back soon.</p>
+    </div>
+  );
+}
+
+export default App;
