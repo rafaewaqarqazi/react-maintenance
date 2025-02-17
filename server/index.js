20b6c0b621b833025afa2180d5ee19fe7ca54833
rafaewaqarqazi <rafaewaqar@gmail.com>
2025-02-17 17:12:43 +0500

    Add: port

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
+require("dotenv").config();
 const path = require("path");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.get("/*", function (req, res) {
